f79acacb9ad80da174189ee9f03080930c235d29
techlead95 <techlead95@gmail.com>
2024-04-03 15:21:28 -0500

    misc

--- database/migrations/2024_04_03_143552_create_misc_table.php
+++ database/migrations/2024_04_03_143552_create_misc_table.php
@@ -12,15 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('misc', function (Blueprint $table) {
+            $table->id();
             $table->string('mi_item_name');
-            $table->string('mi_setup_date');
             $table->string('mi_setup_by');
-            $table->string('mi_modified_date');
             $table->string('mi_modified_by');
             $table->string('mi_o1_translation');
             $table->string('mi_DVI_translation');
             $table->string('mi_o2_add_code');
             $table->string('mi_o1_add_code');
+            $table->timestamps();
         });
     }
 
